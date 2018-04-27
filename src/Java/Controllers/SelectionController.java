8dd30e32b7a41378d31accd669d47fd4fb92c189
zer0 <sahinonur2000@hotmail.com>
2018-04-27 11:32:58 +0300

    Selection page created .gitignore added for out files

---
+++ src/Java/Controllers/SelectionController.java
@@ -0,0 +1,14 @@
+package Java.Controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SelectionController implements Initializable
+{
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //TO DO...
+    }
+}
