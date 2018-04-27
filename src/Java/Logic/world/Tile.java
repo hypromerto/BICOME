bd145148eb22aba9e6f05269f3e3d757f97f3439
hypromerto <aslan-mert99@hotmail.com>
2018-04-27 22:26:42 +0300

    Changed world, added tile class

---
+++ src/Java/Logic/world/Tile.java
@@ -0,0 +1,48 @@
+package world;
+
+/**
+ * 
+ * @author  Mert Aslan
+ * @version 27.04.2018
+ */
+public class Tile 
+{
+	private int row;
+	private int col;
+	private boolean selected;
+	private Organism o;
+	
+	public Tile( int row, int col )
+	{
+		this.row = row;
+		this.col = col;
+		o = null;
+	}
+	
+	public void placeOrganism( Organism o )
+	{
+		this.o = o;
+	}
+	
+	public Organism getOrganism()
+	{
+		return o;
+	}
+	
+	public boolean isEmpty()
+	{
+		return o == null;
+	}
+	
+	public void setSelected( boolean state )
+	{
+		selected = state;
+	}
+	
+	public boolean getSelected()
+	{
+		return selected;
+	}
+	
+	
+}
--- src/Java/Logic/world/World.java
+++ src/Java/Logic/world/World.java
@@ -1,155 +1,167 @@
 package world;
 /**
  * @author Mert Aslan
- * @version 27.4.2018
+ * @version 27.04.2018
  */
+
 import java.util.ArrayList;
 import environment.Environment;
+
 public class World 
 {
-   
-   private final double REPR_THRESHOLD = 1.3;
-   
-   private Organism[][] organisms;
-   private Environment environment;
-   private int round;
-   
-   public World()
-   {
-      organisms  = new Organism[50][50];
-      environment = new Environment();
-      round = 0;
-   }
-   
-   public boolean placeOrganism( int row, int col, Organism o )
-   {
-      // stub
-      return true;
-   }
-   
-   public void nextTurn()
-   {
-      int totalCell;
-      ArrayList<Organism> neighbors;
-      Organism[][] offsprings;
-      Boolean[][] emptySpaces;
-      
-      
-      emptySpaces = new Boolean[9][9];
-      offsprings = new Organism[50][50];
-      neighbors = new ArrayList<Organism>();
-      totalCell = 0;
-      
-      if ( !isGameOver() )
-      {
-         for ( int i = 0; i < organisms.length; i++ )
-         {
-            for ( int j = 0; j < organisms.length; j++ )
-            {
-               if ( organisms[i][j] instanceof Organism )
-               {
-                  for ( int k = 0; k < 9; k++) 
-                  {
-                     int row = i + (k % 3) - 1;
-                     int col = j + (k / 3) - 1;
-                     
-                     if (row >= 0 && row <= organisms.length && col >= 0 && col <= organisms.length && !(row == i && col == j))
-                     {
-                        totalCell++;
-                        
-                        if ( organisms[row][col] instanceof Organism ) 
-                        {
-                           neighbors.add( organisms[row][col ] );
-                        }
-                        else
-                        {
-                           emptySpaces[row][col] = true; //needs a better implementation or a way to find row col indexes with respect to center organism
-                        }
-                     }
-                     
-                  }
-                  
-                  if ( !( neighbors.size() < 2 || neighbors.size() > 3 ) )
-                  {
-                     //if  suitable amount of neighbors, reproduce after calculating reproduction chance
-                     
-                     if( totalCell > neighbors.size() ) //if there are spaces left for offspring
-                     {
-                        if ( neighbors.size() == 2 && 2 * Math.random() > REPR_THRESHOLD )
-                        {
-                           // IMPORTANT: AN IF STATEMENT HERE TO CHECK IF ORGANISMS SURVIVAL RATE IS ENOUGH TO SURVIVE!!!!
-                           int toDo;
-                           
-                           toDo = (int) Math.round( Math.random() );
-                           
-                           if ( organisms[i][j].canReproduce() && neighbors.get( toDo ).canReproduce() )
-                           {
-                              //randomly select a location from emptySpaces array and fill its corresponding index in
-                              //offsprings multi array
-                              
-                              
-                              
-                              offsprings[i][j] = organisms[i][j].reproduce(neighbors.get( toDo ) );
-                              
-                           }  
-                        }
-                        else if ( neighbors.size() == 3 &&  3 * Math.random() > REPR_THRESHOLD )
-                        {
-                           // IMPORTANT: AN IF STATEMENT HERE TO CHECK IF ORGANISMS SURVIVAL RATE IS ENOUGH TO SURVIVE!!!!
-                           
-                        }
-                        
-                        
-                     }
-                     
-                  }
-                  else
-                  {
-                     // die without reproducing
-                     // stub
-                  }
-                  
-                  
-               }
-               
-            }
-         }
-         
-         for ( int i = 0; i < offsprings.length; i++ )
-         {
-            for ( int j = 0; j < offsprings.length; i++ )
-            {
-               if ( organisms[i][j] instanceof Organism )
-               {
-                  if ( !organisms[i][j].canReproduce() ) //incrementing the organisms with cooldown active
-                     organisms[i][j].increaseCooldown();
-                  
-                  organisms[i][j].age();
-               }
-               
-               if ( offsprings[i][j] instanceof Organism )
-               {
-                  organisms[i][j] = offsprings[i][j]; // porting our offsprings back to original organisms array
-                  
-                  offsprings[i][j] = null; // flushing offsprings
-               }
-            }
-         }
-         round++;
-         
-         
-      }
-   }
-   
-   
-   public boolean isGameOver()
-   {
-      // stub
-      return false;
-   }
-   
-   public Environment getEnvironment()
-   {
-      return environment;
-   }
+	
+	private final double REPR_THRESHOLD = 1.3;
+	
+	private Tile [][] tiles;
+	private Environment environment;
+	private int round;
+	
+	public World()
+	{
+		tiles  = new Tile[50][50];
+		environment = new Environment();
+		round = 0;
+		
+		for ( int i = 0; i < tiles.length; i++ )
+		{
+			for( int j = 0; j < tiles.length; j++ )
+			{
+				tiles[i][j] = new Tile(i,j);
+			}
+				
+		}
+	}
+	
+	public boolean placeOrganism( int row, int col, Organism o )
+	{
+		// stub
+		return true;
+	}
+	
+	
+	
+	public void nextTurn()
+	{
+		int totalCell;
+		ArrayList<Organism> neighbors;
+		Organism[][] offsprings;
+		Boolean[][] emptySpaces;
+		
+		
+		emptySpaces = new Boolean[9][9];
+		offsprings = new Organism[50][50];
+		neighbors = new ArrayList<Organism>();
+		totalCell = 0;
+		
+		if ( !isGameOver() )
+		{
+			for ( int i = 0; i < tiles.length; i++ )
+			{
+				for ( int j = 0; j < tiles.length; j++ )
+				{
+					if ( !tiles[i][j].isEmpty() )
+					{
+						for ( int k = 0; k < 9; k++) 
+						{
+							int row = i + (k % 3) - 1;
+							int col = j + (k / 3) - 1;
+							
+							if (row >= 0 && row <= tiles.length && col >= 0 && col <= tiles.length && !(row == i && col == j))
+							{
+								totalCell++;
+								
+								if ( !tiles[row][col].isEmpty() ) 
+								{
+									neighbors.add(  tiles[row][col].getOrganism() );
+								}
+								else
+								{
+									tiles[row][col].setSelected( true ); //needs a better implementation or a way to find row col indexes with respect to center organism
+								}
+							}
+							
+						}
+						
+						if ( !( neighbors.size() < 2 || neighbors.size() > 3 ) )
+						{
+							//if  suitable amount of neighbors, reproduce after calculating reproduction chance
+							
+							if( totalCell > neighbors.size() ) //if there are spaces left for offspring
+							{
+								if ( neighbors.size() == 2 && 2 * Math.random() > REPR_THRESHOLD )
+								{
+									// IMPORTANT: AN IF STATEMENT HERE TO CHECK IF ORGANISMS SURVIVAL RATE IS ENOUGH TO SURVIVE!!!!
+									int toDo;
+									
+									toDo = (int) Math.round( Math.random() );
+									
+											if ( tiles[i][j].getOrganism().canReproduce() && neighbors.get( toDo ).canReproduce() )
+											{
+												//randomly select a location from emptySpaces array and fill its corresponding index in
+												//offsprings multi array
+												
+												
+												
+												offsprings[i][j] = tiles[i][j].getOrganism().reproduce(neighbors.get( toDo ) );
+												
+											}		
+								}
+								else if ( neighbors.size() == 3 &&  3 * Math.random() > REPR_THRESHOLD )
+								{
+									// IMPORTANT: AN IF STATEMENT HERE TO CHECK IF ORGANISMS SURVIVAL RATE IS ENOUGH TO SURVIVE!!!!
+									
+								}
+									
+							
+							}
+							
+						}
+						else
+						{
+							//die without reproducing
+							//stub
+						}
+						
+						
+					}
+					
+				}
+			}
+			
+			for ( int i = 0; i < offsprings.length; i++ )
+			{
+				for ( int j = 0; j < offsprings.length; i++ )
+				{
+					if ( !tiles[i][j].isEmpty() )
+					{
+						if ( tiles[i][j].getOrganism().canReproduce() ) //incrementing the organisms with cooldown active
+							tiles[i][j].getOrganism().increaseCooldown();
+						
+						tiles[i][j].getOrganism().age();
+					}
+					
+					if ( offsprings[i][j] instanceof Organism )
+					{
+						tiles[i][j].placeOrganism(offsprings[i][j]); //porting our offsprings back to original organisms array
+						
+						offsprings[i][j] = null; //flushing offsprings
+					}
+				}
+			}
+			round++;
+			
+			
+		}
+	}
+	
+	
+	public Boolean isGameOver()
+	{
+		
+	}
+	
+	public Environment getEnvironment()
+	{
+		return environment;
+	}
 }
