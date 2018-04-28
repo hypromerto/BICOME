fcdb8d6ebea6b7ef277709b7a2441eb7715493f1
hypromerto <aslan-mert99@hotmail.com>
2018-04-28 14:35:45 +0300

    Made changes to Tile and World class
    Tile class now has a method to kill the organism and get rows and cols.
    
    World class now uses two arraylists to detect alive and empty neighbours, completed reproduction for a center organism with two neighbours, still need to implement the same algorithm to center organisms with 3 neighbours. This time it needs to randomly select a tile from 3 rather than 2 so its algorithm will be a bit different.

--- src/Java/Logic/world/Tile.java
+++ src/Java/Logic/world/Tile.java
@@ -24,6 +24,11 @@ public class Tile
       this.o = o;
    }
    
+   public void killOrganism()
+   {
+	   o = null;
+   }
+   
    public Organism getOrganism()
    {
       return o;
@@ -32,6 +37,16 @@ public class Tile
    public boolean isEmpty()
    {
       return o == null;
+   }
+   
+   public int getRow()
+   {
+	   return row;
+   }
+   
+   public int getCol()
+   {
+	   return col;
    }
    
    public void setSelected( boolean state )
