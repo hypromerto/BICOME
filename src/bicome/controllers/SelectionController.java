1e515ce7189f66d70a078906da3f459a83f7177b
Onur <onur_sahin@chromium.org>
2018-05-07 19:00:32 +0300

    writed method to adding and removing features

--- src/bicome/controllers/SelectionController.java
+++ src/bicome/controllers/SelectionController.java
@@ -1,14 +1,20 @@
 package bicome.controllers;
 
+import bicome.dialogs.FeatureDialog;
+import bicome.logic.feature.Feature;
 import bicome.logic.feature.FeatureBase;
+import bicome.logic.genotype.Genotype;
 import bicome.utils.FeaturePopulator;
 import com.jfoenix.controls.JFXListView;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.StackPane;
 
 import java.awt.event.MouseEvent;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class SelectionController implements Initializable
@@ -17,18 +23,30 @@ public class SelectionController implements Initializable
     private StackPane rootPane;
 
     @FXML
-    JFXListView<FeatureBase> selectedFeaturesListView;
+    JFXListView<Feature> selectedFeaturesListView;
 
     @FXML
     JFXListView<FeatureBase> unSelectedFeaturesListView;
+
+    private ObservableList<Feature> selectedList;
+    private ObservableList<FeatureBase> unSelectedList;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         //Set the listView to the expanded mode (Looks cooler ;) )
         selectedFeaturesListView.setExpanded(true);
         unSelectedFeaturesListView.setExpanded(true);
+
+        //Initialize the observables
+        selectedList = FXCollections.observableArrayList();
+        unSelectedList = FXCollections.observableArrayList();
+
         //Add all features to unselected list
-        FeaturePopulator.populateFeatures(unSelectedFeaturesListView.getItems());
+        FeaturePopulator.populateFeatures(unSelectedList);
+
+        //Link the observables
+        selectedFeaturesListView.setItems(selectedList);
+        unSelectedFeaturesListView.setItems(unSelectedList);
     }
 
     @FXML
@@ -37,7 +55,16 @@ public class SelectionController implements Initializable
         JFXListView<FeatureBase> listView = (JFXListView<FeatureBase>)event.getSource();
 
         FeatureBase selected = listView.getSelectionModel().getSelectedItem();
+        Optional<Genotype> selectedGene = FeatureDialog.showDialog(selected);
+        Feature feature;
 
-        //To Do get selected genotype and create a feature with that genotype then add it to the selectedFeaturesListView
+        if(selectedGene.isPresent()) {
+            feature = new Feature(selected, selectedGene.get());
+            unSelectedList.remove(selected);
+            selectedList.add(feature);
+        }
+        else {
+            System.out.println();
+        }
     }
 }
